3fa7f9837fdeb2e9716fffa4455b4c5658528bb5
Mike Stankiewicz <stankiem89@gmail.com>
2018-03-11 19:30:17 -0600

    added content to all folders for backend

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE boigahz(
+id INT NOT NULL AUTO_INCREMENT,
+burg_name VARCHAR (60) NULL,
+shnarfed BOOLEAN(100) NULL,
+day_eaten TIMESTAMP (30) NULL,
+PRIMARY KEY (id)
+);
+
+ALTER TABLE boigahz ADD INDEX  #12:50 Jay goes over this
+
+SELECT * FROM boigahz;
